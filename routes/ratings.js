de51db46c4f8fec34c05b26dcdf821296bc4f1b8
jianfangbhbbc <jianfang.bladen-hovell@bbc.co.uk>
2021-03-26 18:36:48 +0000

    handlebars loads the data to a html table

--- routes/ratings.js
+++ routes/ratings.js
@@ -5,8 +5,14 @@ const { fetch } = require('./fetch-api')
 /* GET movies listing. */
 router.get('/', async function (req, res, next) {
   const data = await fetch(req.query.mt1, req.query.mt2, req.query.mt3);
-  console.log(data)
-  res.status(200).render('layout', { body: JSON.stringify(data) });
+  const th = '<tr><th>Title</th><th>Year</th><th>Rating</th></tr>'
+  const row1 = `<tr><td>${data[0].title}</td><td>${data[0].year}</td><td>${data[0].rating}</td></tr>`;
+  const row2 = `<tr><td>${data[1].title}</td><td>${data[1].year}</td><td>${data[1].rating}</td></tr>`;
+  const row3 = `<tr><td>${data[2].title}</td><td>${data[2].year}</td><td>${data[2].rating}</td></tr>`;
+
+  res.status(200).render('layout', {
+    body: `${th}${row1}<br/>${row2}<br/>${row3}`
+  });
 });
 
 module.exports = router;
