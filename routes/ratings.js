8c5afd9b123c27eca6d2a57ea22e824433adb656
jianfangbhbbc <jianfang.bladen-hovell@bbc.co.uk>
2021-03-27 14:00:24 +0000

    save to a csv file

--- routes/ratings.js
+++ routes/ratings.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const { fetch } = require('./fetch-api')
+const fs = require('fs')
 
 /* GET movies listing. */
 router.get('/', async function (req, res, next) {
@@ -16,9 +17,26 @@ router.get('/', async function (req, res, next) {
     row2 = `<tr><td>${data[1].title}</td><td>${data[1].rating}</td><td>${data[1].lang}</td><td>${data[1].year}</td><td>${data[1].plot}</td></tr>`;
   if (data && data[2])
     row3 = `<tr><td>${data[2].title}</td><td>${data[2].rating}</td><td>${data[2].lang}</td><td>${data[2].year}</td><td>${data[2].plot}</td></tr>`;
+
+  //save to a csv file
+  const csvStr = saveToCSV(data);
+  // res.status(200).render('layout', {
+  //   body: csvStr
+  // });
   res.status(200).render('layout', {
     body: `${th}${row1}<br/>${row2}<br/>${row3}`
   });
+
 });
 
+const saveToCSV = (data) => {
+  let csvStr = 'Title,Rating,Lnguage,Year,Summary\n';
+
+  for (let d of data) {
+    csvStr += `${d.title},${d.rating},${d.lang.replace(/(,\s*)/g, '.')},${d.year},${d.plot.replace(/(,\s*)/g, '.')}\n`
+  }
+  fs.writeFileSync('./ouput.csv', csvStr)
+  return csvStr;
+
+}
 module.exports = router;
